98a5562b3a7ee5688408f86a0e335a9b04c0c6f1
theiguim <theiguim@gmail.com>
2025-12-15 18:33:12 -0300

    att cases

--- src/components/Cases/Cases.jsx
+++ src/components/Cases/Cases.jsx
@@ -28,7 +28,7 @@ export default function Cases() {
       <div className="gallery-header">
         <SectionHeader tag="Cases Selecionados"
           title="Cases rescentes."
-          align="left" 
+          align="left"
         />
 
         <div className="scroll-hint">
@@ -52,7 +52,7 @@ export default function Cases() {
         <div className="gallery-track" ref={trackRef}>
 
           <a href="https://www.legendarioslascampanas.com" target="_blank" className="project-poster">
-            <img className="poster-img" src="/images/works/lgnd.png" alt="Legendários" />
+            <img className="poster-img" src="/images/cases/lgnd-min.png" alt="Legendários" />
             <div className="poster-info">
               <p className="poster-cat">E-Commerce</p>
               <h3 className="poster-title">Legendários Las Campanas</h3>
@@ -60,35 +60,87 @@ export default function Cases() {
           </a>
 
           <a href="https://fg360transportes.com.br/" target="_blank" className="project-poster">
-            <img className="poster-img" src="/images/works/fg.png" alt="FG360" />
+            <img className="poster-img" src="/images/cases/fg-min.png" alt="FG360" />
             <div className="poster-info">
               <p className="poster-cat">Logística</p>
               <h3 className="poster-title">FG360 Transportes</h3>
             </div>
           </a>
 
-          <a href="https://blackbrian.com/" target="_blank" className="project-poster">
-            <img className="poster-img" src="/images/works/bb.png" alt="Black Brian" />
+          <a href="https://www.resolveonline.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/ro-min.png" alt="FG360" />
             <div className="poster-info">
-              <p className="poster-cat">Landing Page</p>
-              <h3 className="poster-title">Black Brian</h3>
+              <p className="poster-cat">HUB</p>
+              <h3 className="poster-title">Resolve Online</h3>
+            </div>
+          </a>
+
+          <a href="https://www.omneimperium.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/oi-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Contabilidade</p>
+              <h3 className="poster-title">Omni Imperium</h3>
+            </div>
+          </a>
+          <a href="https://www.labraengenharia.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/labra-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Engenharia</p>
+              <h3 className="poster-title">Labra</h3>
             </div>
           </a>
 
           <a href="https://www.silvaneves.com.br/" target="_blank" className="project-poster">
-            <img className="poster-img" src="/images/works/sn.png" alt="Silva Neves" />
+            <img className="poster-img" src="/images/cases/sn-min.png" alt="FG360" />
             <div className="poster-info">
-              <p className="poster-cat">Jurídico</p>
-              <h3 className="poster-title">Silva Neves Advocacia</h3>
+              <p className="poster-cat">Advocacia</p>
+              <h3 className="poster-title">Silva Neves</h3>
+            </div>
+          </a>
+          <a href="https://www.forneria360.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/fornaria-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Delivery</p>
+              <h3 className="poster-title">Fornaria 360º</h3>
+            </div>
+          </a>
+          <a href="https://www.empreendimentosgran.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/granemp-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Construtora</p>
+              <h3 className="poster-title">Gran Empreendimentos</h3>
             </div>
           </a>
 
-          {/* Repetido apenas para mostrar scroll se necessário */}
-          <a href="#" className="project-poster">
-            <img className="poster-img" src="/images/works/bb.png" alt="Exemplo Extra" />
+          <a href="https://www.omegajunior.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/omega-min.png" alt="FG360" />
             <div className="poster-info">
-              <p className="poster-cat">Tecnologia</p>
-              <h3 className="poster-title">Mais Projetos...</h3>
+              <p className="poster-cat">Engenharia</p>
+              <h3 className="poster-title">Ômega Júnior</h3>
+            </div>
+          </a>
+
+          <a href="https://www.advocaciaconrado.com/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/conrado-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Advocacia</p>
+              <h3 className="poster-title">Conrado Advocacia</h3>
+            </div>
+          </a>
+
+          <a href="https://joggybrasil.com.br/" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/joggy-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Produto</p>
+              <h3 className="poster-title">JOGGY Brasil</h3>
+            </div>
+          </a>
+
+          <a href="" target="_blank" className="project-poster">
+            <img className="poster-img" src="/images/cases/realiza-min.png" alt="FG360" />
+            <div className="poster-info">
+              <p className="poster-cat">Imobiliaria</p>
+              <h3 className="poster-title">Realiza Imóveis</h3>
             </div>
           </a>
 
